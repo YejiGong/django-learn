ee049c76edf917a9e4d8ef26c0624305578f5e5c
YejiGong <yejigong00@gmail.com>
2022-06-29 00:10:02 +0900

    first commit

---
+++ proj/main/url.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib import admin
+from . import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('blog/', views.blog, name='blog'),
+    path('blog/<int:pk>/', views.posting, name='posting'),
+    path('blog/new_post/', views.new_post, name='new_post'),
+    path('blog/<int:pk>/remove/', views.remove_post, name='remove_post'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
